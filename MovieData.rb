c44525f9bc3dc46d509b18a51883078ed1065ddc
Saul Tbeile <saultbeile@outlook.com>
2015-01-29 19:42:34 -0500

    Added comments

--- MovieData.rb
+++ MovieData.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class MovieData
 	
-
+	#cant remember if you can use two separate initializers in ruby
 	def initialize(folder,test=nil)
 		if test != nil
 			@training_set = load_data("#{folder}/#{test.to_s}.base")
@@ -13,11 +13,12 @@ class MovieData
 			@genre_set = load_genre("#{folder}/u.item")
 		end
 	end
-	
+	#global attributes
 	attr_reader:training_set
 	attr_reader:test_set
 	attr_reader:genre_set
 
+	#loads data using csv
 	def load_data(file_name)
 		indata = []
 		#csv reader as shown in class; typecasted because I ran into some weird bugs
@@ -27,6 +28,9 @@ class MovieData
 		return indata
 	end
 
+	#for predict I decided to use genres instead of similarity. Not really sure why I did that CSV with | as the col sep
+	#there must be an easier way to add the value at indexes 5-23 to genre_for_movie
+	#fails complexity tests
 	def load_genre(file_name)
 		indata = []
 		CSV.foreach("#{file_name}", col_sep: "|") do |row|
@@ -36,6 +40,7 @@ class MovieData
 		return indata
 	end
 
+	#looks through the training set and if the movie id = m and userof == u then returns the rating at that row
 	def rating(u,m)
 		training_set.each do |row|
 			if row["movie_id"] == m && row["user_id"] == u 
@@ -44,6 +49,8 @@ class MovieData
 		end
 		return 0
 	end
+
+	#gets a list of movies watched by the user and pushes the row and genres to the array
 	def movies(u)
 		user_list = []
 		training_set.each do |row|
@@ -55,6 +62,8 @@ class MovieData
 		
 		return user_list
 	end
+	#creates a list of users that saw the movie and the rating they gave
+	#if used similarity score would use this as the comparison list using most similar in movies-1
 	def viewers(m)
 		viewer_list = []
 		training_set.each do |row|
@@ -64,10 +73,13 @@ class MovieData
 		end
 		return viewer_list
 	end
+
+
 	def predict(u,m)
 		user_list = movies(u)
 		m_genre = genre_set[m-1]["genres"]
 		m_genre_index = []
+		#pushes which genres this movie has
 		m_genre.each_index do |x|
 			if m_genre[x] == 1
 				m_genre_index.push(x)
@@ -77,6 +89,7 @@ class MovieData
 		u_genre_rating = []
 		counter = 0
 		m_genre_index.each do |i|
+			#u_genre_rating[0] is the count, u_genre_rating[1] is the cumulative total
 			u_genre_rating.push([0,0])
 			user_list.each do |row|
 				test_genres = row["genres"]
@@ -88,14 +101,18 @@ class MovieData
 			end
 			counter +=1
 		end
+		#total average
 		average = 0.0
 		u_genre_rating.each do |b,t|
 			if b != 0 && t != 0
 				average += (t.to_f/b.to_f)
 			end
 		end
+		#bugs galore to get here
 		return average/u_genre_rating.length
 	end
+
+	#sends all values that are going to be in the movietest object to an array and returns the movie test object
 	def run_test(k)
 		to_movietest = []
 		(0..k).each do |i|
@@ -110,12 +127,14 @@ end
 
 class MovieTest
 	
+	#initialized base on a list passed by run test
 	def initialize(result_list)
 		@results = result_list
 	end
 	
 	attr_reader:results
 	
+	#gets the mean of error as a float
 	def mean()
 		average = 0.0
 		@results.each do |row|
@@ -124,6 +143,7 @@ class MovieTest
 
 		return average/results.length
 	end
+	#get stddev of the error
 	def stddev()
 		mean = mean()
 		av_error  = 0.0
@@ -133,6 +153,8 @@ class MovieTest
 		tobesquared = av_error/results.length.to_f
 		return Math.sqrt(tobesquared)
 	end
+
+	#get root mean square as a float. think this is where the error occurred
 	def rms
 		sum = 0.0
 		results.each do |row|
@@ -142,6 +164,7 @@ class MovieTest
 		rms = Math.sqrt(sum)
 		return rms
 	end
+	#cast each row to an array and returns it
 	def to_a
 		arr = []
 		results.each do |row|
@@ -152,10 +175,9 @@ class MovieTest
 
 end
 
+#NOTES I HAD A NAN ERROR AT SOME POINT, NOT EXACTLY AN ERROR BUT IT WOULD CHANGE THE AVERAGE TO NAN. DONT REMEMBER WHAT CAUSED IT
 
-
-
-z = MovieData.new("ml-100k",:u1)
+#z = MovieData.new("ml-100k",:u1)
 #puts z.rating(22,377)
 #puts z.movies(22)
 #puts z.viewers(22)
